b21798752ce2d846745873e79f0e3cbdad125b8b
codeblech <my.malikyash@gmail.com>
2025-11-30 18:58:04 +0530

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div style={{ width: "100vw", height: "100vh", backgroundColor: "#170D27" }}>
-      <DomeGallery fitBasis="auto" grayscale={false} fit={0.9}/>
+      <DomeGallery fitBasis="auto" grayscale={false} fit={0.9} minRadius={1000}/>
     </div>
   );
 }
