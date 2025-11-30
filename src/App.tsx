f4ebe715e0d5cf58058e44a9aea9b66038a7ef69
codeblech <my.malikyash@gmail.com>
2025-11-30 23:08:19 +0530

    drag dynamics

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div style={{ width: "100vw", height: "100vh", backgroundColor: "#170D27" }}>
-      <DomeGallery fitBasis="auto" grayscale={false} fit={0.9} minRadius={1000} padFactor={0.2}/>
+      <DomeGallery fitBasis="auto" grayscale={false} fit={0.9} minRadius={1000} padFactor={0.2} dragSensitivity={40} dragDampening={4}/>
     </div>
   );
 }
