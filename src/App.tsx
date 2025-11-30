ee8b442fc93a520d8322d780711cf73b8e8cfb48
codeblech <my.malikyash@gmail.com>
2025-11-30 16:45:08 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import DomeGallery from "@/components/DomeGallery";
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <DomeGallery />
+    </div>
+  );
+}
+
+export default App;
